2ea7684bfc086b508f7ff67d4768f11689b686e3
John Paul Ruf <john.paul.ruf@gmail.com>
2019-06-23 20:55:54 -0500

    Combat engine

--- app/program/gameLogic/npc.js
+++ app/program/gameLogic/npc.js
@@ -7,4 +7,35 @@ class npc {
  
     this.weapon = new weapon();
   }
+
+  attack(npc) {
+
+    const getDamage = new function (statValue, npc) {
+      const d20 = new dice().max = 20;
+      let damage = 0;
+      const hitRoll = d20.roll();
+      if (hitRoll + statValue / 2 > npc.DEX) {
+        damage = damage.weapon.getDamage();
+      }
+
+      return hitRoll === 20 ? damage * 2 : damage;
+    };
+
+    switch (this.weapon.stat) {
+      case 'STR':
+        npc.hp -= getDamage(this.STR, npc);
+        break;
+      case 'DEX':
+        npc.hp -= getDamage(this.DEX, npc);
+        break;
+
+      case 'INT':
+        npc.hp -= getDamage(this.INT, npc);
+        break;
+      default:
+        npc.hp -= getDamage(_.max([this.STR, this.INT, this.DEX]), npc);
+        break;
+
+    }
+  }
 };
--- app/program/gameLogic/player.js
+++ app/program/gameLogic/player.js
@@ -56,39 +56,4 @@ class player extends npc {
           break;
       }
   }
-
-  attack(npc) {
-
-    const getDamage = new function (statValue, npc) {
-      const d20 = new dice().max = 20;
-      let damage = 0;
-      const hitRoll = d20.roll();
-      if (hitRoll + statValue / 2 > npc.DEX) {
-        damage = damage.weapon.getDamage();
-      }
-
-      return hitRoll === 20 ? damage * 2 : damage;
-    };
-
-    switch (this.weapon.stat) {
-      case 'STR':
-        npc.hp -= getDamage(this.STR, npc);
-        break;
-      case 'DEX':
-        npc.hp -= getDamage(this.DEX, npc);
-        break;
-
-      case 'INT':
-        npc.hp -= getDamage(this.INT, npc);
-        break;
-      default:
-        npc.hp -= getDamage(_.max([this.STR, this.INT, this.DEX]), npc);
-        break;
-
-    }
-  }
-
-  defendAgainst(npc) {
-
-  }
 };
