6513654fdb92bb220eda62cb2278418ba2be45f6
John Paul Ruf <john.paul.ruf@gmail.com>
2019-07-05 22:03:38 -0500

    update

--- app/zen.js
+++ app/zen.js
@@ -1,6 +1,5 @@
 this.loader = new Loader();
 this.arenaRPG = new ArenaRPG();
-this.asteroids = new Asteroids();
 
 function setup() {
   this.loader.init();
@@ -9,20 +8,16 @@ function setup() {
   this.arenaRPG.init();
   this.arenaRPG.setup();
 
-  this.asteroids.init();
-  this.asteroids.setup();
 }
 
 function draw() {
   this.loader.draw();
   this.arenaRPG.draw();
-  this.asteroids.draw();
 }
 
 function mousePressed(event) {
   this.loader.mouseClick(event);
   this.arenaRPG.mouseClick(event);
-  this.asteroids.mouseClick(event);
   return false;
 }
 
